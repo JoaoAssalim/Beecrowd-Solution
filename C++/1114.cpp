08ddc8996512566d8d8061db2e161a425e0bc762
João Assalim <joassalim@gmail.com>
2022-08-03 16:42:57 -0300

    Create 1114.cpp

---
+++ C++/1114.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main() {
+    while(1){
+        int senha;
+        cin >> senha;
+        if(senha == 2002){
+            cout << "Acesso Permitido\n";
+            break;
+        }
+        else{cout << "Senha Invalida\n";}
+    }
+    
+}
