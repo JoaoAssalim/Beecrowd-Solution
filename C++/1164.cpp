f688a0ea4c04cba4fda5be0841c7c50a479e02ec
João Assalim <joassalim@gmail.com>
2022-08-03 20:13:41 -0300

    Create 1164.cpp

---
+++ C++/1164.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+ 
+    int n;
+    scanf("%d", &n);
+    for(int i = 0; i < n; i++){
+        int num, soma = 0;
+        scanf("%d", &num);
+        for(int y = 1; y < num; y++){
+            if(num%y == 0){
+                soma += y;
+            }
+        }
+        if(num == soma){
+            printf("%d eh perfeito\n", num);
+        }else{
+            printf("%d nao eh perfeito\n", num);
+        }
+    }
+}
