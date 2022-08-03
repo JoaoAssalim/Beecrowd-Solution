cf0b458710fc7c605f1b5e64897a6d6344d0d2cf
João Assalim <joassalim@gmail.com>
2022-08-03 13:37:07 -0300

    Create 1008.cpp

---
+++ C++/1008.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int a;
+    float b,c;
+    cin >> a >> b >> c;
+    cout << "NUMBER = " << a << endl;
+    printf("SALARY = U$ %.2f\n", b*c);
+}
