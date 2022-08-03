506554ad9f0a30f892b3ba3f06a8232e337052e8
João Assalim <joassalim@gmail.com>
2022-08-03 20:08:09 -0300

    Create 1156.cpp

---
+++ C++/1156.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+ 
+    float s = 1;
+    float a = 3, b = 2;
+    
+    while(a <= 39){
+        s += a/b;
+        a += 2;
+        b *= 2;
+    }
+    printf("%.2f\n", s);
+}
