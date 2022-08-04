cbc35c44c71a6d66eaa6be392869814aa12605f3
João Assalim <joassalim@gmail.com>
2022-08-04 12:21:02 -0300

    Create 1865.cpp

---
+++ C++/1865.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+ 
+    int x;
+    cin >> x;
+    
+    for(int i = 0; i < x; i++){
+        string nome;
+        int poder;
+        cin >> nome >> poder;
+        if(nome == "Thor"){cout << "Y\n";}
+        else{cout << "N\n";}
+    }
+}
