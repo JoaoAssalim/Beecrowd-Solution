339938121549f8b6c32b2eca36c7fd219c839663
João Assalim <joassalim@gmail.com>
2022-08-04 10:56:44 -0300

    Create 1759.cpp

---
+++ C++/1759.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    
+    int n;
+    cin >> n;
+    for(int i = 0; i <n-1;i++){
+        cout << "Ho ";
+    }
+    cout << "Ho!\n";
+}
