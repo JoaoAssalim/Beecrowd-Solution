b103f6445b674eb17e2575825780a3b3eb49900d
João Assalim <joassalim@gmail.com>
2022-08-04 12:18:52 -0300

    Create 1864.cpp

---
+++ C++/1864.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+ 
+    char palavra[50] = "LIFE IS NOT A PROBLEM TO BE SOLVED";
+    int x;
+    cin >> x;
+    
+    for(int i = 0; i < x; i++){
+        cout << palavra[i];
+    }
+    cout << endl;
+}
