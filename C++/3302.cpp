860d4acdb72a43a228969917fde5f8456dbb1b07
João Assalim <joassalim@gmail.com>
2022-08-04 12:29:08 -0300

    Create 3302.cpp

---
+++ C++/3302.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main() {
+
+    int a;
+    cin >> a;
+    for (int contador = 1; contador <= a; contador++) {
+        int result;
+        cin >> result;
+        printf("resposta %d: %d\n", contador, result);
+    }
+}
