ff04240548b063076f21ee1190bd550ed7722b06
João Assalim <joassalim@gmail.com>
2023-06-27 18:10:25 -0300

    Update 2930.java

--- Java/2930.java
+++ Java/2930.java
@@ -4,13 +4,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String s = scanner.nextLine();
-        String[] ns = s.split(" ");
+        String[] ns = s.split(" ")
         int total = Integer.parseInt(ns[1]) - Integer.parseInt(ns[0]);
 
         if(total >= 3){
             System.out.println("Muito bem! Apresenta antes do Natal!");
         }else if(total > 0 && total <= 2){
-            if(b < 23){
+            if(Integer.parseInt(ns[1]) < 23){
                 System.out.println("Parece o trabalho do meu filho!");
                 System.out.println("TCC Apresentado!");
             }else{
