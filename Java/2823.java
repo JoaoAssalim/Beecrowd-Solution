015e1ccbfc7107bd5bb10ca486c0594f0d29637b
João Assalim <joassalim@gmail.com>
2023-06-27 18:31:10 -0300

    Update 2823.java

--- Java/2823.java
+++ Java/2823.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        float count = 0;
+        double count = 0;
         int n = scanner.nextInt();
         for(int i = 0; i < n; i++){
-            float a = scanner.nextFloat(), b = scanner.nextFloat();
+            double a = scanner.nextDouble(), b = scanner.nextDouble();
             count += a/b;
         }
         if(count <= 1){
