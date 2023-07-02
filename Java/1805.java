f0ddc9582c31846b55fc521ecb500bb22a9ce5c3
João Assalim <joassalim@gmail.com>
2023-07-02 15:13:30 -0300

    Create 1805.java

---
+++ Java/1805.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long a = sc.nextLong(), b = sc.nextLong();
+        System.out.println(((a+b) * (b-a+1)) / 2);
+        
+    }
+}
