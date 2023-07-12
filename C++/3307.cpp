e6e2090b62a3e268b3078f6fee78067be87a9196
João Assalim <joassalim@gmail.com>
2023-07-12 08:19:14 -0300

    Create 3307.cpp

---
+++ C++/3307.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n; scanf("%d", &n);
+    for(int i = 0; i < n; i++){
+        int x; scanf("%d", &x);
+        double r = sqrt(x/(4*3.14));
+        if(r < 12)
+            printf("vermelho = R$ %.2lf\n", x*0.09);
+        else if(r <= 15)
+            printf("azul = R$ %.2lf\n", x*0.07);
+        else
+            printf("amarelo = R$ %.2lf\n", x*0.05);
+    }
+    
+}
